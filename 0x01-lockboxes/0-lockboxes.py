c7ee1e912076d9190f434dcbb19fa8378f710c71
pericles001 <periclesadjovi@outlook.fr>
2022-06-21 15:35:58 +0100

    Move test file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you. Each box is numbered sequentially
+from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+def canUnlockAll(boxes):
+    """
+     a method that determines if all the boxes can be opened.
+
+    :param boxes:
+    :return: True or False
+    """
