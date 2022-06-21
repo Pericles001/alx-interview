4f8eb8e4f17b7234062ed65c2e0c8b1c1ebaa001
pericles001 <periclesadjovi@outlook.fr>
2022-06-21 16:12:29 +0100

    Edit file with solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you. Each box is numbered sequentially
-from 0 to n - 1 and each box may contain keys to the other boxes.
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially
+from 0 to n - 1 and each box may
+contain keys to the other boxes.
 """
+
 
 def canUnlockAll(boxes):
     """
@@ -11,3 +14,14 @@ def canUnlockAll(boxes):
     :param boxes:
     :return: True or False
     """
+    if not boxes or type(boxes) is not list:
+        return False
+
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
