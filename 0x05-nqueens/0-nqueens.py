da0c982753ba03f7b85bf7eb32257666dabfe9fc
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-07-22 02:39:16 +0000

    Edit file with solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -20,21 +20,35 @@ if n_q < 4:
     exit(1)
 
 
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+
 def attack_queen(square, queen):
-    """
-    attack queen method
-    """
+    '''self descriptive'''
     (row1, col1) = square
     (row2, col2) = queen
     return (row1 == row2) or (col1 == col2) or\
-    abs(row1 - row2) == abs(col1 - col2)
+        abs(row1 - row2) == abs(col1 - col2)
 
-def safe_queen(square, queens):
-    """
-    Safe queen method
-    """
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
     for queen in queens:
-        if attack_queen(square, queen):
+        if attack_queen(sqr, queen):
             return False
-        return True
+    return True
 
+
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
