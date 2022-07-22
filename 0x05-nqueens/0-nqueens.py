7ed17adb1fab633895c78b936e0e090193c9f2c8
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-07-22 02:24:17 +0000

    Edit file to test user inputs

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,3 +2,19 @@
 """
 N queens
 """
+
+import sys
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N\n')
+    exit(1)
+
+try:
+    n_q = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+
+if n_q < 4:
+    print('N must be at least 4')
+    exit(1)
