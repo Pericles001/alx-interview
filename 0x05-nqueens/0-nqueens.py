cf115766d87ff9c725b8224ea40c2019af81a1a1
Pericles001 <periclesadjovi@gmail.com>
2022-07-22 15:23:09 +0100

    Edit file

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""
-N queens
-"""
-
+""" N queens"""
 import sys
 
 if len(sys.argv) != 2:
-    print('Usage: nqueens N\n')
+    print("Usage: nqueens N")
     exit(1)
 
 try:
     n_q = int(sys.argv[1])
 except ValueError:
-    print('N must be a number')
+    print("N must be a number")
     exit(1)
 
 if n_q < 4:
-    print('N must be at least 4')
+    print("N must be at least 4")
     exit(1)
 
 
