33f06d3e0302b995903b47e29e151d3e92bb675d
Pericles001 <periclesadjovi@gmail.com>
2022-07-27 21:42:35 +0100

    Fix semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,17 +4,17 @@ const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
 async function starwarsCharacters (filmID) {
-    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-    const response = await (await request(endpoint)).body;
-    response = JSON.parse(response)
-    const characters = response.characters;
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  const response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-    for (let i = 0; i < characters.length; i++) {
-        const urlCharacter = characters[i];
-        let character = await (await request(urlCharacter)).body;
-        character = JSON.parse(character);
-        console.log(character.name);
-    }
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
 starwarsCharacters(filmID);
