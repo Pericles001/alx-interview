56b37713eb5f1cbbe6efed15e5da504a83c45db2
Pericles001 <periclesadjovi@gmail.com>
2022-07-27 21:21:06 +0100

    Edit file with function to display movie name

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
+
+async function starwarsCharacters (filmID) {
+    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+    const response = await (await request(endpoint)).body;
+    response = JSON.parse(response)
+    const characters = response.characters;
+
+    for (let i = 0; i < characters.length; i++) {
+        const urlCharacter = characters[i];
+        let character = await (await request(urlCharacter)).body;
+        character = JSON.parse(character);
+        console.log(character.name);
+    }
+}
+starwarsCharacters(filmID);
