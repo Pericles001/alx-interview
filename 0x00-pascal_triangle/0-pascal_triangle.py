13b51ecf0f0482e22ab157fc7662ebb85ebd3de4
Pericles001 <periclesadjovi@gmail.com>
2022-11-02 13:12:39 +0100

    Edit file to solve problem

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,4 +11,10 @@ def pascal_triangle(n):
           the Pascal’s triangle of n
          Returns an empty list if n <= 0
     """
-
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(n - 1):
+        triangle.append([1] + [triangle[i][j] + triangle[i][j + 1]
+                               for j in range(i)] + [1])
+    return triangle
